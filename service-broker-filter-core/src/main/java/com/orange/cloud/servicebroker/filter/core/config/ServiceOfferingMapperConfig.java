8ba860d114f3261546b69fd3183ae98f90e3b331
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-09-14 05:42:57 -0700

    Update ServiceOfferingMapperConfig.java
    Signed-off-by: Alexander  Petree <apetree1001@email.phoenix.edu>

--- service-broker-filter-core/src/main/java/com/orange/cloud/servicebroker/filter/core/config/ServiceOfferingMapperConfig.java
+++ service-broker-filter-core/src/main/java/com/orange/cloud/servicebroker/filter/core/config/ServiceOfferingMapperConfig.java
@@ -1,62 +1,24 @@
-package com.orange.cloud.servicebroker.filter.core.config;
-
-import com.orange.cloud.servicebroker.filter.core.service.mapper.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @author Sebastien Bortolussi
- */
-public class ServiceOfferingMapperConfig {
-
-    @Configuration
-    protected static class DefaultServiceOfferingMapperConfig {
-
-        @Bean
+package com.orange.cloud.servicebroker.filter.core.config; import com.orange.cloud.servicebroker.filter.core.service.mapper.*;import org.springframework.beans.factory.annotation.Value;import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;import org.springframework.context.annotation.Bean;import org.springframework.context.annotation.Configuration;/*** @author Sebastien Bortolussi*/@Alexander petree 
+public class ServiceOfferingMapperConfig {@Configuration
+    protected static class DefaultServiceOfferingMapperConfig {@Bean
         @ConditionalOnMissingBean
-        CatalogMapper catalogMapper() {
-            return new DefaultCatalogMapper();
-        }
-
+        CatalogMapper catalogMapper() {return new DefaultCatalogMapper();}
         @Bean
         @ConditionalOnMissingBean
         ServiceInstanceRequestMapper serviceInstanceRequestMapper() {
-            return new DefaultServiceInstanceRequestMapper();
-        }
-
+            return new DefaultServiceInstanceRequestMapper();}
         @Bean
         @ConditionalOnMissingBean
-        ServiceInstanceBindingRequestMapper serviceInstanceBindingRequestMapper() {
-            return new DefaultServiceInstanceBindingRequestMapper();
-        }
-    }
-
-    @Configuration
+       ServiceInstanceBindingRequestMapper serviceInstanceBindingRequestMapper() {return new DefaultServiceInstanceBindingRequestMapper();} }  @Configuration
     @ConditionalOnProperty(value = "broker.filter.serviceoffering.suffix")
-    protected static class SuffixedServiceOfferingMapperConfig {
+    protected static class SuffixedServiceOfferingMapperConfig {@Bean
+        String suffix(@Value("${broker.filter.serviceoffering.suffix}") String suffix) { return suffix;}@Bean
+        CatalogMapper catalogMapper(String suffix) {return new SuffixedCatalogMapper(suffix);}
+        @Bean
+        ServiceInstanceRequestMapper serviceInstanceRequestMapper(String suffix) {return new SuffixedServiceInstanceRequestMapper(suffix);}
 
         @Bean
-        String suffix(@Value("${broker.filter.serviceoffering.suffix}") String suffix) {
-            return suffix;
-        }
-
-        @Bean
-        CatalogMapper catalogMapper(String suffix) {
-            return new SuffixedCatalogMapper(suffix);
-        }
-
-        @Bean
-        ServiceInstanceRequestMapper serviceInstanceRequestMapper(String suffix) {
-            return new SuffixedServiceInstanceRequestMapper(suffix);
-        }
-
-        @Bean
-        ServiceInstanceBindingRequestMapper serviceInstanceBindingRequestMapper(String suffix) {
-            return new SuffixedServiceInstanceBindingRequestMapper(suffix);
-        }
+    ServiceInstanceBindingRequestMapper serviceInstanceBindingRequestMapper(String suffix) {return new SuffixedServiceInstanceBindingRequestMapper(suffix); }
     }
 
 }
